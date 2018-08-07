6b549e802bb8f8718f8259933387456f32117c96
FelixPcll <felix@df.ufscar.br>
2018-08-06 21:31:17 -0300

    Add documentation to image_resize.py

--- image_resize.py
+++ image_resize.py
@@ -1,6 +1,7 @@
 """To run the classifier, all the images must be in the same size.
 This script will scan the folders and resize all the images from them
-and put all in a new folder
+and put all in a new folder, separating all classes in different folders
+inside a new folder called 'classes'
 
 """
 
@@ -9,6 +10,17 @@ import cv2
 
 
 def resize_square_and_save(image_current_path, shape, save_path):
+    """Resize the image to a square shape keeping it's original proportion
+    and filling the rest of image with black pads to keep it square.
+
+    :param image_current_path: the local path where the image is.
+    :type image_current_path: OS string
+    :param shape: number of pixels per axis of the converted image
+    :type shape: int
+    :param save_path: path where the converted image must be saved
+    :type save_path: OS string
+    """
+
     original_image = cv2.imread(image_current_path, 1)
     original_size = original_image.shape[:2]  # (height, width)
 
@@ -34,22 +46,33 @@ def resize_square_and_save(image_current_path, shape, save_path):
 
 
 def run(folder, shape):
+    """run a script that get all images inside a directory with
+    labelled directories, square and resize the images and save them
+    inside a new folder keeping the labelled folders.
+
+    :param folder: the folder where the pictures are located
+    :type folder: string
+    :param shape: how much pixels will the output images have on each axis
+    :type shape: int
+    :raises FileNotFoundError: the folder declared doesn't exist
+    """
+
     root0 = os.getcwd()
     dir0 = os.path.join(root0, folder)
     if os.path.isdir(dir0):
-        root1, dir1, file1 = [i for i in os.walk(dir0)][0]
+        root1, dir1, _ = [i for i in os.walk(dir0)][0]
         for sub_folder in dir1:
             # Create the new path to drop the formatted figures
-            new_path = os.path.join(root0, 'formatted images/'+sub_folder)
+            new_path = os.path.join(root0, 'classes/'+sub_folder)
             try:
                 os.makedirs(new_path)
-            except:
+            except BaseException:
                 pass
 
             old_path = os.path.join(root1, sub_folder)
             walk = [i for i in os.walk(old_path)][1:]
 
-            for root, folder_list, img_list in walk:
+            for root, _, img_list in walk:
                 for img in img_list:
                     im_path = os.path.join(root, img)
                     save_path = os.path.join(new_path, img)
